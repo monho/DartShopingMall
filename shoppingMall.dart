cea6b6bfc692c8de7e3f6a1c8bb6ef2c14076c35
MoonHo <75512752+monho@users.noreply.github.com>
2025-03-14 08:50:23 +0900

    [2025.03.14 문준호] 프로그램 종료 다시 물어보기 기능 구현
    [2025.03.14 문준호] 프로그램 종료 다시 물어보기 기능 구현

--- shoppingMall.dart
+++ shoppingMall.dart
@@ -7,9 +7,9 @@ class ShoppingMall {
   final ProductList productList = ProductList();
   final Cart cart = Cart();
   final PriceCalculator priceCalculator = PriceCalculator();
-
+  bool isRunning = true;
   void run() {
-    bool isRunning = true;
+    
     while (isRunning) {
       print("\n" + "-" * 70);
       print("[1] 상품 목록 보기 / [2] 장바구니에 담기 / [3] 장바구니 총 가격 보기 / [6]장바구니 비우기 / [7] 프로그램 종료");
@@ -27,16 +27,32 @@ class ShoppingMall {
         case "3":
           priceCalculator.showTotalPrice(cart);
           break;
-        case "4":
-          print("이용해 주셔서 감사합니다 ~ 안녕히 가세요 !");
-          isRunning = false;
-
         case "6":
           cart.clearCart();
+        case "7":
+          this.exitProgram();
           break;
         default:
           print("❌ 지원하지 않는 기능입니다 ! 다시 시도해 주세요 ..");
       }
     }
   }
+
+  void exitProgram()
+  {
+    stdout.write("정말 종료하시겠습니까?");
+    String? choice = stdin.readLineSync()?.trim();
+
+    if(choice == "8")
+    {
+      print("이용해 주셔서 감사합니다 ~ 안녕히 가세요 !");
+      isRunning = false;  
+    }
+    else
+    {
+      print("종료하지 않습니다.");
+      return;
+    }
+  }
+
 }
